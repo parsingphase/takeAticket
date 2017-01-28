dd6f3534f0fb6c3d23f2d9ebbcc9c69f916c54ba
Richard George <richard@phase.org>
2017-01-28 21:53:42 +0000

    Migrate management ajax

--- src/Phase/TakeATicketBundle/Controller/AjaxController.php
+++ src/Phase/TakeATicketBundle/Controller/AjaxController.php
@@ -16,6 +16,15 @@ class AjaxController extends Controller
 {
     use DataStoreAccessTrait;
 
+    const MANAGER_REQUIRED_ROLE = 'ROLE_ADMIN';
+    const BAND_IDENTIFIER_BAND_NAME = 1;
+    const BAND_IDENTIFIER_PERFORMERS = 2;
+
+    /**
+     * @var int Whether to identify band by its name or a list of performers
+     */
+    protected $bandIdentifier = self::BAND_IDENTIFIER_PERFORMERS;
+
     /**
      * @return JsonResponse
      *
@@ -56,9 +65,109 @@ class AjaxController extends Controller
 
     }
 
+    public function useTicketAction(Request $request)
+    {
+//        $this->setJsonErrorHandler();
+
+        $this->denyAccessUnlessGranted(self::MANAGER_REQUIRED_ROLE);
+
+        $id = $request->get('ticketId');
+        $res = $this->getDataStore()->markTicketUsedById($id);
+        //FIXME fetch ticket with extra data
+        if ($res) {
+            $jsonResponse = new JsonResponse(['ok' => 'ok']);
+        } else {
+            $jsonResponse = new JsonResponse(['ok' => 'fail'], 500);
+        }
+        return $jsonResponse;
+    }
+
     public function remotesRedirectAction()
     {
         return new JsonResponse($this->getDataStore()->getSetting('remotesUrl'));
     }
 
+    public function saveTicketAction(Request $request)
+    {
+//        $this->setJsonErrorHandler();
+        $this->denyAccessUnlessGranted(self::MANAGER_REQUIRED_ROLE);
+
+        $title = $request->get('title');
+        $songKey = $request->get('songId');
+        $band = $request->get('band') ?: []; // band must be array even if null (as passed by AJAX if no performers)
+        $private = $request->get('private') === 'true' ? 1 : 0;
+        $blocking = $request->get('blocking') === 'true' ? 1 : 0;
+        $existingTicketId = $request->get('existingTicketId');
+
+        $song = null;
+        $songId = null;
+
+        if (preg_match('/^[a-f0-9]{6}$/i', $songKey)) {
+            $song = $this->getDataStore()->fetchSongByKey($songKey);
+        } elseif (preg_match('/^\d+$/', $songKey)) {
+            $song = $this->getDataStore()->fetchSongById($songKey);
+        }
+
+        if ($song) {
+            $songId = $song['id'];
+        }
+
+        if (!$title) {
+            $title = null;
+        }
+
+        if ($existingTicketId) {
+            $ticketId = $existingTicketId;
+        } else {
+            $ticketId = $this->getDataStore()->storeNewTicket($title, $songId);
+        }
+
+        // update even new tickets so that we can add any new columns easily
+        $updated = ['title' => $title, 'songId' => $songId, 'blocking' => $blocking, 'private' => $private];
+
+//        $this->app['logger']->debug("Updating ticket", $updated);
+        $this->getDataStore()->updateTicketById(
+            $ticketId,
+            $updated
+        );
+
+        if ($this->bandIdentifier === self::BAND_IDENTIFIER_PERFORMERS) {
+            $this->getDataStore()->storeBandToTicket($ticketId, $band);
+        }
+
+        $ticket = $this->getDataStore()->fetchTicketById($ticketId);
+
+        $ticket = $this->getDataStore()->expandTicketData($ticket);
+
+        $responseData = [
+            'ticket' => $ticket,
+            'performers' => $this->getDataStore()->generatePerformerStats()
+        ];
+
+        if ($ticketId) {
+            $jsonResponse = new JsonResponse($responseData);
+        } else {
+            $jsonResponse = new JsonResponse($responseData, 500);
+        }
+        return $jsonResponse;
+
+
+    }
+
+
+    public function deleteTicketAction(Request $request)
+    {
+//        $this->setJsonErrorHandler();
+
+        $this->denyAccessUnlessGranted(self::MANAGER_REQUIRED_ROLE);
+
+        $id = $request->get('ticketId');
+        $res = $this->getDataStore()->deleteTicketById($id);
+        if ($res) {
+            $jsonResponse = new JsonResponse(['ok' => 'ok']);
+        } else {
+            $jsonResponse = new JsonResponse(['ok' => 'fail'], 500);
+        }
+        return $jsonResponse;
+    }
 }
