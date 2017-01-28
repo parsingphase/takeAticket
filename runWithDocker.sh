31469d60ca5b5cd67b71e39434356a99dd94a023
Richard George <richard@phase.org>
2017-01-28 11:17:52 +0000

    Define docker port mapping

--- runWithDocker.sh
+++ runWithDocker.sh
@@ -2,7 +2,10 @@
 
 which docker>/dev/null || echo 'Please install docker from https://www.docker.com'
 
-docker run -i -t -P takeaticket
+docker run -i -t -p 8080:8080 takeaticket
+
+echo "Listening on http://localhost:8080"
+
 #php -S 0:8080 -t www/
 
 # WORKS with ENTRYPOINT php -S 0:8080 -t www
