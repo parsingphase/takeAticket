eddbddb89fbc1b0826af734b1451a0365c550704
Richard George <richard@phase.org>
2017-06-06 08:47:38 +0100

    Add and independently secure dummy API endpoints

---
+++ src/Phase/TakeATicketBundle/Controller/ApiController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wechsler
+ * Date: 06/06/2017
+ * Time: 08:30
+ */
+
+namespace Phase\TakeATicketBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiController extends Controller
+{
+    public function articlesAction()
+    {
+        $articles = array('article1', 'article2', 'article3');
+        return new JsonResponse($articles);
+    }
+
+    public function userAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if($user) {
+            return new JsonResponse(array(
+                'id' => $user->getId(),
+                'username' => $user->getUsername()
+            ));
+        }
+
+        return new JsonResponse(array(
+            'message' => 'User is not identified'
+        ));
+
+    }
+}
